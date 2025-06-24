28a24d95999e0d4f45a1bd8dd97351f9a5267082
Siddharth39 <megaberu81@gmail.com>
2025-06-24 21:18:07 +0530

    In mking

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -91,3 +92,32 @@ def post_share(request, post_id):
             'sent':sent
         }
     )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id = post_id,
+        status = Post.Status.PUBLISHED        
+    )
+
+    comment = None
+    form = CommentForm(data = request.POST)
+    
+    if form.is_valid():
+        comment = form.save(commit = False)
+        comment.post = post
+        comment.save()
+    
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post':post,
+            'form':form,
+            'comment':comment
+        }
+    )
+
+
