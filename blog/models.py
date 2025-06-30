c69520a0112f4e5f215be723e0080ba33d50ec3c
Siddharthh39 <megaberu81@gmail.com>
2025-06-30 11:20:32 +0530

    Added the init comment section

--- blog/models.py
+++ blog/models.py
@@ -79,7 +79,7 @@ class Comment(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    active = models.BinaryField(default=True)
+    active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['created']
