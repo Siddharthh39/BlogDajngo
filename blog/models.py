82e6df5c073911a49c6c0d8c7f4f8d58d5031c9b
Siddharthh39 <megaberu81@gmail.com>
2025-07-01 17:41:38 +0530

    Adding lables

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -47,6 +48,8 @@ class Post(models.Model):
     published = PublishedManager()
     drafted = DraftManager()
     
+    tags = TaggableManager()
+
     class Meta:
         ordering = ['-publish']
         indexes = [
